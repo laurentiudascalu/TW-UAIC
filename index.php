174baf84472f5b46fe906bdaac252a1f59fc616c
dulceanumaria <48507106+dulceanumaria@users.noreply.github.com>
2019-05-19 00:17:23 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,152 +1,16 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$base_url = 'https://';
-	} else {
-		$base_url = 'http://';
-	}
-	$base_url .= $_SERVER['HTTP_HOST'].'/adwise/';
-	$url       = $_SERVER['REQUEST_URI'];
-	$activ     = 'home';
-	$activCont = '';
+	require('./declarations.php');
 
-
-	//ini_set('max_execution_time', 0);
-
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "twuaic";
-
-	// Create connection
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	// Check connection
-	if (!$conn) {
-	    die("Connection failed: " . mysqli_connect_error());
-	}
-
-	if(isset($_POST['editeazadate'])){
-		$sql='';
-		$ok=0;
-		if(isset($_POST['nume']) && $_POST['nume']!=''){
-			if($ok==0){
-				$sql = "UPDATE useri SET `nume_complet`='".$_POST['nume']."'";
-				$ok=1;
-			}elseif($ok==1){
-				$sql.=", `nume_complet`='".$_POST['nume']."'";
-			}
-		}
-		if(isset($_POST['mail']) && $_POST['mail']!=''){
-			if($ok==0){
-				$sql = "UPDATE useri SET `mail`='".$_POST['mail']."'";
-				$ok=1;
-			}elseif($ok==1){
-				$sql.=", `mail`='".$_POST['mail']."'";
-			}
-		}
-		if(isset($_POST['telefon']) && $_POST['telefon']!=''){
-			if($ok==0){
-			$sql = "UPDATE useri SET `tel`='".$_POST['telefon']."'";
-			$ok=1;
-		}elseif($ok==1){
-			$sql.=", `tel`='".$_POST['telefon']."'";
-		}
-		}
-		$sql.=" where `id`='".$_POST['id']."'";
-			if ($conn->query($sql) === TRUE) {
-			    echo "Profil modificat";
-			} else {
-			    echo "Error updating record: " . $conn->error;
-			}
-			header('Location: '.$base_url.'contultau');
-			exit;
-	}
-	if(isset($_POST['schimbaparola'])){
-		$sql='';
-		if(isset($_POST['parolaveche']) && $_POST['parolaveche']!='' && isset($_POST['parolanoua']) && $_POST['parolanoua']!='' && isset($_POST['reparolanoua']) && $_POST['reparolanoua']!='' && $_POST['parolanoua']==$_POST['reparolanoua']){
-				$sql = "UPDATE useri SET `parola`='".$_POST['parolanoua']."'";
-		}
-		/*if ($conn->query($sql) === TRUE) {
-		    echo "Record updated successfully";
-		} else {
-		    echo "Error updating record: " . $conn->error;
-		}
-		print_r($sql);exit;*/
-	}
-
-	if(isset($_POST['complprofil'])){
-		$sql='';
-		$ok=0;
-		if(isset($_POST['oras']) && $_POST['oras']!=''){
-			if($ok==0){
-			$sql = "UPDATE useri SET `oras`='".$_POST['oras']."'";
-			$ok=1;
-		}elseif($ok==1){
-			$sql.=", `oras`='".$_POST['oras']."'";
-		}
-		}
-		if(isset($_POST['adresa']) && $_POST['adresa']!=''){
-			if($ok==0){
-			$sql = "UPDATE useri SET `adresa`='".$_POST['adresa']."'";
-			$ok=1;
-		}elseif($ok==1){
-			$sql.=", `adresa`='".$_POST['adresa']."'";
-		}
-		}
-		if(isset($_POST['datanastere']) && $_POST['datanastere']!=''){
-			if($ok==0){
-			$sql = "UPDATE useri SET `data_nasterii`='".$_POST['datanastere']."'";
-			$ok=1;
-		}elseif($ok==1){
-			$sql.=", `data_nasterii`='".$_POST['datanastere']."'";
-		}
-		}
-		if(isset($_POST['culoarepref']) && $_POST['culoarepref']!=''){
-			if($ok==0){
-			$sql = "UPDATE useri SET `culoare_preferata`='".$_POST['culoarepref']."'";
-			$ok=1;
-		}elseif($ok==1){
-			$sql.=", `culoare_preferata`='".$_POST['culoarepref']."'";
-		}
-		}
-		if(isset($_POST['mancarepref']) && $_POST['mancarepref']!=''){
-			if($ok==0){
-			$sql = "UPDATE useri SET `mancare_preferata`='".$_POST['mancarepref']."'";
-			$ok=1;
-		}elseif($ok==1){
-			$sql.=", `mancare_preferata`='".$_POST['mancarepref']."'";
-		}
-		}
-		if(isset($_POST['descriere']) && $_POST['descriere']!=''){
-			if($ok==0){
-			$sql = "UPDATE useri SET `descriere`='".$_POST['descriere']."'";
-			$ok=1;
-		}elseif($ok==1){
-			$sql.=", `descriere`='".$_POST['descriere']."'";
-		}
-		}
-		$sql.=" where `id`='".$_POST['id']."'";
-
-		if ($conn->query($sql) === TRUE) {
-		    echo "Profil modificat";
-		} else {
-		    echo "Error updating record: " . $conn->error;
-		}
-	}
-
-	if(isset($_POST['stergecont'])){
-		$sql='';
-		if(isset($_POST['parola']) && $_POST['parola']!=''){
-				$sql = "UPDATE useri SET `del`='1' where `id`='".$_POST['id']."'";
-		}
-		if ($conn->query($sql) === TRUE) {
-		    echo "Cont sters";
-		} else {
-		    echo "Error updating record: " . $conn->error;
-		}
-	}
-
-
-	if(strstr($url,'/despre')){
+	require('./autentificare.php');
+	 
+	if(strstr($url,'/delogare')){
+		$sql="UPDATE login SET `del` = '1' WHERE `cookie` = '".$_COOKIE['login']."'";
+		$result = mysqli_query($conn, $sql);
+		unset($_COOKIE['login']);
+    	setcookie('login', '', time() - 3600);
+	}elseif(strstr($url,'/autentificare')){
+		require('./autentificare.php');
+	}elseif(strstr($url,'/despre')){
 		require('./pages/despre.php');
 	}elseif(strstr($url,'/adaugaintrebare')){
 		$activ='adaugaintrebare';
@@ -155,26 +19,6 @@
 		require('./pages/contnou.php');
 	}elseif(strstr($url,'/contultau')){
 		$activCont = 'contultau';
-		$sql="select * from `useri` where `del` = '0' and `id` = '1'";
-		$result = mysqli_query($conn, $sql.'');
-		$useri=array();
-		while($row = mysqli_fetch_assoc($result)) { 
-			$useri['id']=$row['id'];
-			$useri['mail']=$row['mail'];
-			$useri['parola']=$row['parola'];
-			$useri['nume_complet']=$row['nume_complet'];
-			$useri['tel']=$row['tel'];
-			$useri['admin']=$row['admin'];
-			$useri['oras']=$row['oras'];
-			$useri['judet']=$row['judet'];
-			$useri['adresa']=$row['adresa'];
-			$useri['data_nasterii']=$row['data_nasterii'];
-			$useri['culoare_preferata']=$row['culoare_preferata'];
-			$useri['mancare_preferata']=$row['mancare_preferata'];
-			$useri['animal_preferat']=$row['animal_preferat'];
-			$useri['descriere']=$row['descriere'];
-			$useri['del']=$row['del'];
-		}
 		require('./pages/contultau.php');
 	}elseif(strstr($url,'/editraspuns')){
 		require('./pages/editraspuns.php');
@@ -205,26 +49,6 @@
 		require('./pages/planuriviitor.php');
 	}elseif(strstr($url,'/profil')){
 		$activCont = 'profil';
-		$sql="select * from `useri` where `del` = '0' and `id` = '1'";
-		$result = mysqli_query($conn, $sql.'');
-		$useri=array();
-		while($row = mysqli_fetch_assoc($result)) { 
-			$useri['id']=$row['id'];
-			$useri['mail']=$row['mail'];
-			$useri['parola']=$row['parola'];
-			$useri['nume_complet']=$row['nume_complet'];
-			$useri['tel']=$row['tel'];
-			$useri['admin']=$row['admin'];
-			$useri['oras']=$row['oras'];
-			$useri['judet']=$row['judet'];
-			$useri['adresa']=$row['adresa'];
-			$useri['data_nasterii']=$row['data_nasterii'];
-			$useri['culoare_preferata']=$row['culoare_preferata'];
-			$useri['mancare_preferata']=$row['mancare_preferata'];
-			$useri['animal_preferat']=$row['animal_preferat'];
-			$useri['descriere']=$row['descriere'];
-			$useri['del']=$row['del'];
-		}
 		require('./pages/profil.php');
 	}elseif(strstr($url,'/raspunsuriletale')){
 		$activCont = 'raspunsuriletale';
@@ -239,21 +63,14 @@
 		require('./pages/statistici.php');
 	}elseif(strstr($url,'/stergecont')){
 		$activCont = 'stergecont';
-		$sql="select * from `useri` where `del` = '0' and `id` = '1'";
-		$result = mysqli_query($conn, $sql.'');
-		$useri=array();
-		while($row = mysqli_fetch_assoc($result)) { 
-			$useri['id']=$row['id'];
-			$useri['mail']=$row['mail'];
-			$useri['parola']=$row['parola'];
-			$useri['del']=$row['del'];
-		}
 		require('./pages/stergecont.php');
 	}elseif(strstr($url,'/taskindeplinite')){
 		require('./pages/taskindeplinite.php');
 	}elseif(strstr($url,'/top')){
 		$activ='top';
 		require('./pages/top.php');
+	}elseif(strstr($url,'/p')){
+		require('./p.php');
 	}else{
 		require('./pages/home.php');
 		exit;
