e681dfa12a263f30c531cb5beec41cea5f1a600f
dulceanumaria <48507106+dulceanumaria@users.noreply.github.com>
2019-06-10 00:37:27 +0300

    Add files via upload

--- pages/editraspuns.php
+++ pages/editraspuns.php
@@ -7,22 +7,41 @@
 			</div>
 			<div class="box full">
 				<div class="formular">
-					<form action="p">
-						<div class="textSus">
-							Data : 19-03-2019 22:30 <br/>
-							Utilizator : Dulceanu Marina <br/>
-							Intrebare : <a href="<?php echo $base_url; ?>intrebareAdmin">Un site pentru paleta de culori?</a><br/>
-						</div>
+					<form action="<?php echo $base_url; ?>p" method="post">
+							<div class="linieForm">
+								<div class="label">
+									Postat de: 
+								</div>
+								<div class="input">
+									<?php echo $raspuns['nume_complet'];?>
+ 								</div>
+							</div>
+							<div class="linieForm">
+								<div class="label">
+									Data: 
+								</div>
+								<div class="input">
+									<?php echo $raspuns['data']; ?>
+								</div>
+							</div>
+							<div class="linieForm">
+								<div class="label">
+									Intrebare: 
+								</div>
+								<div class="input">
+									<a href="<?php echo $base_url.'intrebare/'.$raspuns['id_intrebare']; ?>">Link</a>
+								</div>
+							</div>								
 						<div class="linieForm">
 							<div class="input">
-								<textarea class="height170">Salut!
-Sunt o multime de site-uri bune care te pot ajuta.
-Unul pe care il folosesc eu, si il recomand este coolors.co</textarea> 
+								<textarea name="raspuns" class="height170" placeholder="Raspuns"><?php echo $raspuns['text']; ?></textarea> 
 							</div>
 						</div>
 						<div class="linieForm">
 							<div class="submit">
-						 		<input type="submit" value="Modifica raspuns" name="modificaraspuns">
+								<input type="hidden" name="id_raspuns" value="<?php echo $raspuns['id'] ?>">
+								<input type="hidden" name="id_intrebare" value="<?php echo $raspuns['id_intrebare'] ?>">
+						 		<input type="submit" value="Modifica raspuns" name="editraspuns">
 						 	</div>
 						</div>
 					</form>
---
+++ pages/intrebariacc.php
@@ -0,0 +1,47 @@
+<?php include('./pages/header.php'); ?>
+<?php include('./pages/sus.php'); ?>
+	<div class="content">
+		<div class="container">
+			<div class="titluPrimaPagina">
+				<span>Intrebari neacceptate</span>
+			</div>
+			<?php if($intrebari!=FALSE && mysqli_num_rows($intrebari) > 0){
+		     while($row = mysqli_fetch_assoc($intrebari)) {
+		     	$taguri = getTaguri(-1,'',$row['id']); ?>
+				<a href="<?php echo $base_url; ?>intrebare/<?php echo $row['id']; ?>" class="intrebare">
+					<div class="intrebareTitlu"><?php echo $row['titlu']; ?><span class="blackC"> - postata de: </span> <?php echo $row['nume_complet']; ?></div>
+					<div class="intrebareContent">
+						<?php echo $row['text']; ?>
+					</div>
+					<div class="intrebareHashtaguri">
+						<?php if(mysqli_num_rows($taguri) > 0){
+		     			while($row2 = mysqli_fetch_assoc($taguri)) { ?>
+		     				<div class="hashtag">#<?php echo $row2['tag']; ?></div>
+		     			<?php } } ?>
+					</div>
+					<div class="likeDislike">
+						<div onclick="window.location.replace('<?php echo $base_url; ?>like/intrebare/<?php echo $row['id']; ?>'); return false;" class="like"><i class="fas fa-thumbs-up"></i> <?php echo $row['nrLike']; ?></div>
+						<div onclick="window.location.replace('<?php echo $base_url; ?>like/intrebare/<?php echo $row['id']; ?>/1'); return false;" class="dislike"><i class="fas fa-thumbs-down"></i> <?php echo $row['nrDisLike']; ?></div>
+					</div>
+				</a>
+		<?php } ?>
+			<div class="butoanePaginare">
+				<a href="" title="Prima Pagina"><i class="fas fa-angle-double-left"></i><i class="fas fa-angle-double-left"></i></a>
+				<a href="" title="Pagina Anterioara"><i class="fas fa-angle-double-left"></i></a>
+				<select class="inputSelect w40 mr5">
+					<option value="1" selected="selected">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+				</select>
+				<a href="" title="Pagina Urmatoare"><i class="fas fa-angle-double-right"></i></a>
+				<a href="" title="Ultima Pagina"><i class="fas fa-angle-double-right"></i><i class="fas fa-angle-double-right"></i></a>
+			</div>
+		<?php }else{ ?>
+			<span class="intrebare">
+				<div class="intrebareContent">Nu s-au gasit rezultate pentru filtrarea aplicata.</div>
+			</span>
+		<?php } ?>
+		</div>
+ 	</div>
+<?php include('./pages/jos.php'); ?>
+<?php include('./pages/footer.php'); ?>
