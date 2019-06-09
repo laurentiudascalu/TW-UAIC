ae29ab723618ee736ae81e79ddd47212496fbb38
laurentiudascalu <48380374+laurentiudascalu@users.noreply.github.com>
2019-06-10 00:35:21 +0300

    Add files via upload

--- pages/intrebare.php
+++ pages/intrebare.php
@@ -3,10 +3,12 @@
 	<div class="container">
 		<div class="content">
 			<div class="boxPrimaPagina">
-				<div class="likeDislike">
-					<a href="<?php echo $base_url; ?>like/intrebare/<?php echo $intrebare['id']; ?>"><div class="like"><i class="fas fa-thumbs-up"></i> <?php echo $intrebare['nrLike']; ?></div></a>
-					<a href="<?php echo $base_url; ?>like/intrebare/<?php echo $intrebare['id']; ?>/1"><div class="dislike"><i class="fas fa-thumbs-down"></i> <?php echo $intrebare['nrDisLike']; ?></div></a>
-				</div>
+				<?php if($user['id'] > 0){ ?>
+					<div class="likeDislike">
+						<a href="<?php echo $base_url; ?>like/intrebare/<?php echo $intrebare['id']; ?>"><div class="like"><i class="fas fa-thumbs-up"></i> <?php echo $intrebare['nrLike']; ?></div></a>
+						<a href="<?php echo $base_url; ?>like/intrebare/<?php echo $intrebare['id']; ?>/1"><div class="dislike"><i class="fas fa-thumbs-down"></i> <?php echo $intrebare['nrDisLike']; ?></div></a>
+					</div>
+				<?php } ?>
 				<div class="titluPrimaPagina intrebareTitluPagina areLike"><?php echo $intrebare['titlu']; ?><span class="blackC"> - postata de: </span> <?php echo $intrebare['nume_complet']; ?></div>
 				<div class="intrebarePaginaContent mb20">
 					<?php echo $intrebare['text']; ?>
@@ -48,16 +50,32 @@
 				<div class="contentPrimaPagina pt30">
 					<?php if(mysqli_num_rows($raspunsuri) > 0){
 		     		while($row = mysqli_fetch_assoc($raspunsuri)) { ?>
-		     			<div class="intrebare nohover <?php echo (($row['acceptat']==1)?'raspunsCorect':''); ?>">
+		     			<div class="intrebare nohover <?php echo (($row['acceptat']==1)?'raspunsCorect':''); ?> <?php echo (($user['admin']>0 || ($user['id'] == $intrebare['id_user'] && $intrebare['rezolvata']==0))?'pb50':''); ?>">
 							<div class="titluRaspuns areLike"><?php echo $row['data']; ?> - <span class="redC"><?php echo $row['nume_complet']; ?> </span> a scris:</div>
 							<br/>
 							<div class="intrebareContent">
 								<?php echo $row['text']; ?>
 							</div>
-							<div class="likeDislike">
-								<a href="<?php echo $base_url.'like/raspuns/'.$row['id']; ?>" class="like"><i class="fas fa-thumbs-up"></i> <?php echo $row['nrLike']; ?></a>
-								<a href="<?php echo $base_url.'like/raspuns/'.$row['id'].'/1'; ?>" class="dislike"><i class="fas fa-thumbs-down"></i> <?php echo $row['nrDisLike']; ?></a>
-							</div>
+							<?php if($user['id'] > 0){ ?>
+								<div class="likeDislike">
+									<a href="<?php echo $base_url.'like/raspuns/'.$row['id']; ?>" class="like"><i class="fas fa-thumbs-up"></i> <?php echo $row['nrLike']; ?></a>
+									<a href="<?php echo $base_url.'like/raspuns/'.$row['id'].'/1'; ?>" class="dislike"><i class="fas fa-thumbs-down"></i> <?php echo $row['nrDisLike']; ?></a>
+								</div>
+							<?php } ?>
+							<?php if($user['admin'] > 0 || ($user['id'] == $intrebare['id_user'] && $intrebare['rezolvata']==0)){ ?>
+								<div class="intrebareAdmin">
+									<?php if($user['id'] == $intrebare['id_user'] && $intrebare['rezolvata']==0){ ?>
+										<div onclick="if(confirm('Esti sigur ca vrei sa declari raspunsul castigator?')){ window.location.replace('<?php echo $base_url."winRaspuns/".$row['id']; ?>'); } return false;" class="win"><i class="fas fa-certificate"></i> Castigator</div>
+									<?php } ?>
+									<?php if($user['admin'] > 0){ ?>
+										<?php if($row['acc'] == 0){ ?>
+											<div onclick="if(confirm('Esti sigur ca vrei sa accepti raspunsul?')){ window.location.replace('<?php echo $base_url."accRaspuns/".$row['id']; ?>'); } return false;" class="accepta"><i class="fas fa-check"></i> Accepta</div>
+										<?php } ?>
+										<div onclick="if(confirm('Esti sigur ca vrei sa stergi raspunsul?')){ window.location.replace('<?php echo $base_url."stergeraspuns/".$row['id']; ?>'); } return false;" class="refuza"><i class="fas fa-times"></i> Sterge</div>
+										<div onclick="window.location.replace('<?php echo $base_url.'editraspuns/'.$row['id']; ?>'); return false;" class="veziInfo"><i class="far fa-eye"></i> Edit</div>
+									<?php } ?>
+								</div>
+							<?php } ?>
 						</div>
 		     		<?php } } ?>
 				</div>
--- pages/raspunsuriletale.php
+++ pages/raspunsuriletale.php
@@ -6,54 +6,47 @@
 			<div class="titluContent">
 				<span>Raspunsurile tale</span>
 			</div>
-			<div class="intrebare nohover raspunsCorect">
-						<div class="titluRaspuns areLike">La 19-03-2019 22:30 - <span class="redC">Dulceanu Marina </span> a scris:</div>
-						<div class="intrebareContent">
-							Salut!<br/>
-							Sunt o multime de site-uri bune care te pot ajuta.<br/>
-							Unul pe care il folosesc eu, si il recomand este <a href="https://coolors.co/" target="_blank">coolors.co</a>
-						</div>
-						<div class="likeDislike">
-							<div class="like"><i class="fas fa-thumbs-up"></i> 10</div>
-							<div class="dislike"><i class="fas fa-thumbs-down"></i> 1</div>
-						</div>
+			<?php if($raspunsuri!=FALSE && mysqli_num_rows($raspunsuri) > 0){
+		     while($row = mysqli_fetch_assoc($raspunsuri)) { ?>
+				<div class="intrebare nohover <?php echo (($row['acceptat'])?'raspunsCorect':''); ?> <?php echo (($user['admin']>0)?'pb50':''); ?>">
+					<div class="titluRaspuns areLike">La <?php echo $row['data']; ?> - <span class="redC"><?php echo $row['nume_complet']; ?> </span> a scris:</div>
+					<br/>
+					<div class="intrebareContent">
+							<?php echo $row['text']; ?>
 					</div>
-					<div class="intrebare nohover">
-						<div class="titluRaspuns">La 19-03-2019 22:30 - <span class="redC">Dulceanu Marina </span> a scris:</div>
-						<div class="intrebareContent">
-							Salut!<br/>
-							Sunt o multime de site-uri bune care te pot ajuta.<br/>
-							Unul pe care il folosesc eu, si il recomand este <a href="https://coolors.co/" target="_blank">coolors.co</a>
-						</div>
+					<?php if($user['id'] > 0){ ?>
 						<div class="likeDislike">
-							<div class="like"><i class="fas fa-thumbs-up"></i> 10</div>
-							<div class="dislike"><i class="fas fa-thumbs-down"></i> 1</div>
+							<a href="<?php echo $base_url.'like/raspuns/'.$row['id']; ?>" class="like"><i class="fas fa-thumbs-up"></i> <?php echo $row['nrLike']; ?></a>
+							<a href="<?php echo $base_url.'like/raspuns/'.$row['id'].'/1'; ?>" class="dislike"><i class="fas fa-thumbs-down"></i> <?php echo $row['nrDisLike']; ?></a>
 						</div>
-					</div>
-					<div class="intrebare nohover">
-						<div class="titluRaspuns">La 19-03-2019 22:30 - <span class="redC">Dulceanu Marina </span> a scris:</div>
-						<div class="intrebareContent">
-							Salut!<br/>
-							Sunt o multime de site-uri bune care te pot ajuta.<br/>
-							Unul pe care il folosesc eu, si il recomand este <a href="https://coolors.co/" target="_blank">coolors.co</a>
+					<?php } ?>
+					<?php if($user['admin'] > 0){ ?>
+						<div class="intrebareAdmin">
+							<?php if($row['acc'] == 0){ ?>
+								<div onclick="if(confirm('Esti sigur ca vrei sa accepti raspunsul?')){ window.location.replace('<?php echo $base_url."accRaspuns/".$row['id']; ?>'); } return false;" class="accepta"><i class="fas fa-check"></i> Accepta</div>
+									<?php } ?>
+							<div onclick="if(confirm('Esti sigur ca vrei sa stergi raspunsul?')){ window.location.replace('<?php echo $base_url."stergeraspuns/".$row['id']; ?>'); } return false;" class="refuza"><i class="fas fa-times"></i> Sterge</div>
+							<div onclick="window.location.replace('<?php echo $base_url.'editraspuns/'.$row['id']; ?>'); return false;" class="veziInfo"><i class="far fa-eye"></i> Edit</div>
 						</div>
-						<div class="likeDislike">
-							<div class="like"><i class="fas fa-thumbs-up"></i> 10</div>
-							<div class="dislike"><i class="fas fa-thumbs-down"></i> 1</div>
-						</div>
-					</div>
-					<div class="intrebare nohover">
-						<div class="titluRaspuns">La 19-03-2019 22:30 - <span class="redC">Dulceanu Marina </span> a scris:</div>
-						<div class="intrebareContent">
-							Salut!<br/>
-							Sunt o multime de site-uri bune care te pot ajuta.<br/>
-							Unul pe care il folosesc eu, si il recomand este <a href="https://coolors.co/" target="_blank">coolors.co</a>
-						</div>
-						<div class="likeDislike">
-							<div class="like"><i class="fas fa-thumbs-up"></i> 10</div>
-							<div class="dislike"><i class="fas fa-thumbs-down"></i> 1</div>
-						</div>
-					</div>
+					<?php } ?>
+				</div>
+			<?php } ?>
+			<div class="butoanePaginare">
+				<a href="" title="Prima Pagina"><i class="fas fa-angle-double-left"></i><i class="fas fa-angle-double-left"></i></a>
+				<a href="" title="Pagina Anterioara"><i class="fas fa-angle-double-left"></i></a>
+				<select class="inputSelect w40 mr5">
+					<option value="1" selected="selected">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+				</select>
+				<a href="" title="Pagina Urmatoare"><i class="fas fa-angle-double-right"></i></a>
+				<a href="" title="Ultima Pagina"><i class="fas fa-angle-double-right"></i><i class="fas fa-angle-double-right"></i></a>
+			</div>
+		<?php }else{ ?>
+			<span class="intrebare">
+				<div class="intrebareContent">Nu ai nici un raspuns dat.</div>
+			</span>
+		<?php } ?>
 		</div>
  	</div>
 <?php include('./pages/jos.php'); ?>
